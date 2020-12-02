e1426f1116c9411a61fd50117cb3817ef64a7003
Jacob Musser <tug42490@temple.edu>
2020-12-02 02:44:33 -0500

    Implemented a working word filter, not perfect. Bot currently filters it's own messages too. Code refactor needed.

--- index.js
+++ index.js
@@ -1,22 +1,96 @@
+const fs = require('fs');
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+let blockedWords = [];
+
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-  if (msg.content === '!help') {
-    msg.reply('What can I do for you?');
-  }
-  if(msg.content.startsWith('!clear')){
-    msg.channel.messages.fetch({ limit: 100 }).then(messages => {
-      msg.channel.bulkDelete(messages)
-    });
-  }
+client.on('message', message => {
+	for (var i = 0; i < blockedWords.length; i++) {
+		if (message.content.includes(blockedWords[i])) {
+			message.delete();
+			message.channel.send('Sorry, that message included a banned word.');
+		}
+	}
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	if (commandName === 'banword') {
+		if (!args.length) {
+			let reply = 'You didn\'t provide any arguments, ${message.author}!';
+			reply += '\nThe proper usage would be: \'${prefix}${command.name} wordToBan\'';
+			message.channel.send(reply);
+		}
+		else if (!blockedWords.includes(args[0])) {
+			blockedWords.push(args[0]);
+			message.channel.send('The word ' + args[0] + ' has been added to the filter list.');
+		}
+	}
+	if (commandName === 'seebannedwords') {
+		let reply = 'Banned Words: ';
+		for (var i = 0; i < blockedWords.length; i++) {
+			reply += '\n' + blockedWords[i];
+		}
+		message.channel.send(reply);
+	}
+
+	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+	
+	if (!command) return;
+	
+	if (command.guildOnly && message.channel.type === 'dm') {
+		return message.reply('I can\'t execute that command inside a DM.');
+	}
+	
+	if (command.args && !args.length) {
+		let reply = 'You didn\'t provide any arguments, ${message.author}!';
+		
+		if (command.usage) {
+			reply += '\nThe proper usage would be: \'${prefix}${command.name} ${command.usage}\'';
+		}
+		return message.channel.send(reply);
+	}
+	
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+	
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+	
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+		
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply('Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \'${command.name}\' command.');
+		}
+	}
+	
+	try {
+		command.execute(message, args);
+	}
+	catch (error) {
+		console.error(error);
+		message.reply('There was an error trying to execute that command.');
+	}
 });
 
-client.login('Nzc1MDY5MDgyMDMzODQ4NDAy.X6g9VQ.GhYff6X4hWZOm_bKlIuEfQ7c8bg');
+client.login(token);
